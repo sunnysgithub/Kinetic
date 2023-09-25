9e13feab4b725c35340fd6a8b6631bed143bc048
Sunny Singh <sunnysingh@posteo.de>
2023-09-25 22:47:16 +0200

    Initial Commit

---
+++ Kinetic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kinetic
+//
+//  Created by Sunny Singh on 25.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kinetic/KineticApp.swift
@@ -0,0 +1,17 @@
+//
+//  KineticApp.swift
+//  Kinetic
+//
+//  Created by Sunny Singh on 25.09.23.
+//
+
+import SwiftUI
+
+@main
+struct KineticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
