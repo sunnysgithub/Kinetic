bc7d4476c5cd00cb3d7eca07f2e00e343e95d59b
Sunny Singh <sunnysingh@posteo.de>
2023-09-25 23:32:24 +0200

    Nice interruptible kinetic animation

--- Kinetic/ContentView.swift
+++ Kinetic/ContentView.swift
@@ -1,21 +1,56 @@
-//
-//  ContentView.swift
-//  Kinetic
-//
-//  Created by Sunny Singh on 25.09.23.
-//
-
 import SwiftUI
+import Wave
 
 struct ContentView: View {
+    @State var offsetStart: CGSize = .zero
+    @State var isDragging: Bool = false
+    @State var offset: CGSize = .zero
+    @State var offsetAnimator = SpringAnimator<CGSize>(spring: .defaultAnimated)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        RoundedRectangle(cornerRadius: 12)
+            .fill(.blue)
+            .frame(width:100, height: 100)
+            .offset(offset)
+            .gesture(dragGesture)
+            .onAppear {
+                offsetAnimator.value = offset
+                offsetAnimator.valueChanged = { value in
+                    offset = value
+                }
+            }
+            .overlay {
+                Text("OFFSET: \(offset.height)").monospaced()
+            }
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                if !isDragging {
+                    isDragging = true
+                    offsetStart = offset
+                }
+                offsetAnimator.target = offsetStart + value.translation
+                offsetAnimator.mode = .nonAnimated
+                offsetAnimator.start()
+            }
+            .onEnded { value in
+                isDragging = false
+                offsetAnimator.velocity = value.velocity
+                offsetAnimator.target = .zero
+                offsetAnimator.mode = .animated
+                offsetAnimator.start()
+            }
+    }
+}
+
+extension CGSize {
+    static func + (lhs: CGSize, rhs: CGSize) -> CGSize {
+        CGSize(
+            width: lhs.width + rhs.width,
+            height: lhs.height + rhs.height
+        )
     }
 }
 
